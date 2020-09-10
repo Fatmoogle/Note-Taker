9bf0ad23807be136b9c3053129fe8762173cd6c7
Alex Varela <alexvar93@gmail.com>
2020-09-10 18:00:54 -0400

    added process.env.PORT for heroku deployment

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const fs = require("fs");
 
-const PORT = 8080;
+const PORT = process.env.PORT || 3001;
 
 // Creates a server using Express
 const app = express();
